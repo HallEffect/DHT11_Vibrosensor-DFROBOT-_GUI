62eb3d91eda253cc6a2d7be7e3ed8241ed118316
Alexander Teleshov <alexander.teleshov@gmail.com>
2013-10-20 19:09:26 +0400

    add c# files

---
+++ VibrosensorGUI/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Vibro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.HumTextBlock = new System.Windows.Forms.Label();
+            this.TempTextBlock = new System.Windows.Forms.Label();
+            this.DataTextBlock = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(50, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(50, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(146, 100);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chart1.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))))};
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Вибрация";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(717, 351);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "График колебаний";
+            this.chart1.Titles.Add(title1);
+            // 
+            // HumTextBlock
+            // 
+            this.HumTextBlock.AutoSize = true;
+            this.HumTextBlock.Font = new System.Drawing.Font("Segoe UI Semilight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.HumTextBlock.Location = new System.Drawing.Point(122, 32);
+            this.HumTextBlock.Name = "HumTextBlock";
+            this.HumTextBlock.Size = new System.Drawing.Size(107, 25);
+            this.HumTextBlock.TabIndex = 4;
+            this.HumTextBlock.Text = "Влажность:";
+            // 
+            // TempTextBlock
+            // 
+            this.TempTextBlock.AutoSize = true;
+            this.TempTextBlock.Font = new System.Drawing.Font("Segoe UI Semilight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TempTextBlock.Location = new System.Drawing.Point(322, 32);
+            this.TempTextBlock.Name = "TempTextBlock";
+            this.TempTextBlock.Size = new System.Drawing.Size(126, 25);
+            this.TempTextBlock.TabIndex = 5;
+            this.TempTextBlock.Text = "Температура:";
+            // 
+            // DataTextBlock
+            // 
+            this.DataTextBlock.AutoSize = true;
+            this.DataTextBlock.Font = new System.Drawing.Font("Segoe UI Semilight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DataTextBlock.Location = new System.Drawing.Point(559, 32);
+            this.DataTextBlock.Name = "DataTextBlock";
+            this.DataTextBlock.Size = new System.Drawing.Size(70, 25);
+            this.DataTextBlock.TabIndex = 6;
+            this.DataTextBlock.Text = "Время:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 127);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(128, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 487);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.DataTextBlock);
+            this.Controls.Add(this.TempTextBlock);
+            this.Controls.Add(this.HumTextBlock);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ФРЭЛА МАИ Каф. 403";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label HumTextBlock;
+        private System.Windows.Forms.Label TempTextBlock;
+        private System.Windows.Forms.Label DataTextBlock;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ VibrosensorGUI/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO.Ports;
+
+namespace Vibro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string[] portnames = SerialPort.GetPortNames();
+
+            //serialPort1.PortName = "COM3";
+            //serialPort1.Open();
+            for (int i = 0; i < portnames.Length; i++)
+            {
+                comboBox1.Items.Add(portnames[i]);
+            }
+            
+            if (portnames.Length == 0)
+            {
+                comboBox1.Items.Add("Нет подключенных устройств");
+                comboBox1.SelectedItem = "Нет подключенных устройств";
+                flag = true;
+            }
+            else
+            { 
+                comboBox1.SelectedItem = portnames[0];
+                flag = false;
+            }
+            
+        }
+
+        bool flag = false;
+        delegate void labelTextDelegateTEMP(string text1, string text2);
+        delegate void labelTextDelegateVIBRO(UInt32 text3);
+        string InputFromCOM, TempStringInput, HumidityStringInput;
+        public UInt32 VibroInputInt;
+
+
+        public void StartButton_Click(object sender, EventArgs e)
+        {
+            if (flag) 
+            {
+                MessageBox.Show("Нет устройств, подключенных к COM порту");
+            }
+            else
+            {
+                serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                serialPort1.Open();
+                Thread th =  new Thread(output);
+                th.IsBackground = true;
+                th.Start();
+
+                chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+                chart1.ChartAreas[0].CursorX.IsUserEnabled = true;
+                chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+                chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            }
+
+        }
+
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+
+            /*
+            Microsoft.Office.Interop.Excel.Application EXCEL = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook ObjWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet;
+            //Книга.
+            ObjWorkBook = EXCEL.Workbooks.Add(System.Reflection.Missing.Value);
+            //Таблица.
+            ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook.Sheets[1];
+
+            //Значения [y - строка,VibroInputInt - столбец]
+            ObjWorkSheet.Cells[1, 1] = "Время";
+            ObjWorkSheet.Cells[1, 2] = "Температура";
+            ObjWorkSheet.Cells[1, 3] = "Влажность";
+
+            
+           // ObjWorkSheet.Cells[i, 1] = DateTime.Now.ToString("HH:mm:ss");
+            serialPort1.WriteLine("2");
+
+            serialPort1.WriteLine("2");
+            //ObjWorkSheet.Cells[i, 2] = serialPort1.ReadLine();//"Температура"; //serialPort1.ReadLine();
+            InputFromCOM = serialPort1.ReadLine();
+            serialPort1.WriteLine("1");
+            h = serialPort1.ReadLine();
+
+            textBox1.Text = "Температура " + InputFromCOM + "\n";
+            textBox1.Text =textBox1.Text+ "Влажность " + h + "\n";
+            //ObjWorkSheet.Cells[i, 3] = serialPort1.ReadLine();//"Влажность";//serialPort1.ReadLine();
+
+           // i++;
+           // EXCEL.Visible = true;
+           // EXCEL.UserControl = true;
+            */
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+        }
+
+        public void output()
+        {
+            while (true)
+            {
+                   InputFromCOM = serialPort1.ReadLine();
+                if (string.Concat(InputFromCOM[0], InputFromCOM[1]) == "TE")
+                {
+                    TempStringInput = string.Concat(InputFromCOM[4], InputFromCOM[5]);
+                    HumidityStringInput = string.Concat(InputFromCOM[9], InputFromCOM[10]);
+                    LabelText(TempStringInput, HumidityStringInput);
+                }
+                else
+                {
+                    VibroInputInt = Convert.ToUInt32(InputFromCOM);
+                    ChartGraph(VibroInputInt);
+                }
+             
+            }
+        }
+
+        private void LabelText(string HumidityOutput, string TempOutput)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new labelTextDelegateTEMP(LabelText), new object[] { HumidityOutput, TempOutput});
+                return;
+            }
+            else
+            {
+                HumTextBlock.Text = "Влажность: "+HumidityOutput;
+                TempTextBlock.Text = "Температура: " + TempOutput;
+                DataTextBlock.Text = DateTime.Now.ToString("HH:mm:ss");
+            }
+        }
+
+        private void ChartGraph(UInt32 VibroOutput)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new labelTextDelegateVIBRO(ChartGraph), new object[] { VibroOutput });
+                return;
+            }
+            else
+            {            
+                DataTextBlock.Text = DateTime.Now.ToString("HH:mm:ss");
+                chart1.Series[0].Points.AddXY(DateTime.Now.ToString("HH:mm:ss"), VibroOutput);
+            }
+        }
+
+
+    }
+}
